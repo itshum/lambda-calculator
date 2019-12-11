0797815c811e47ce68bb4cc443dc5dbb3cda4dbe
Humayun Rashid <humayun@Humayuns-iMac.local>
2019-12-11 13:14:46 -0500

    creating new functions to implement operators. Function for addNumbers to add numbers together

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
@@ -19,13 +19,22 @@ function App() {
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
+  // I want to have the button click affect the display on the calculator
+  const [displayValue, setDisplayValue] = useState("0");
+  
+  // Creating new function to help add numbers. Take number as argument. We set our DisplayValue and add the number, so it displays current value of number and adds numbers after click.
+
+  const addNumbers = (number) => {
+    setDisplayValue(displayValue => displayValue + number);
+  }
+
   return (
     <div className="container">
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files 
-        Added Display tag to display calculator screen component w/ initial number of 0*/}
-        <Display number={0}/>
+        Added Display tag to display calculator screen component w/ initial number of 0 but then change it to DisplayValue variable*/}
+        <Display number={displayValue}/>
         <Numbers />
         <Operators /> 
         <Specials />
--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NumberButton = (props) => {
   return (
-    <button>
+    <button onClick={() =>}>
       {/* Display a button element rendering the data being passed down from the parent container on props 
       Add button tag and create .text props*/}
       {props.text}
